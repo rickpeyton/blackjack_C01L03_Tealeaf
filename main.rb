4c57859bcbcd8da94306a7204fe2fb8b3615073a
Rick Peyton <peytorb@gmail.com>
2015-01-23 04:47:41 -0600

    added chrome fix

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'pry'
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'dzbcgyrj'
 
 BLACKJACK_AMOUNT = 21
 DEALER_HIT_TO = 17
